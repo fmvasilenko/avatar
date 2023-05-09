7efa33590b5c148d5fe95fd2c546b149f01985aa
fmvasilenko <fm.vasilenko@yandex.ru>
2023-05-09 17:50:37 +0300

    smaller screens adaptation

--- src/app.ts
+++ src/app.ts
@@ -12,14 +12,20 @@ const blockContainer = div({ id: 'block-container', className: 'blockContainer' 
 document.body.append(blockContainer);
 // TEMPORARY! REMOVE BEFORE RELEASE!
 
-const imageContainer = div({ id: 'image-container', className: 'Avatar__imageContainer' });
-blockContainer.append(imageContainer);
+const avatarContainer = div({ className: 'Avatar' });
+blockContainer.append(avatarContainer);
+
+const imageContainer = div({ className: 'Avatar__imageContainer' });
+avatarContainer.append(imageContainer);
+
+const image = div({ id: 'image-container', className: 'Avatar__image' });
+imageContainer.append(image);
 
 const render = (urls: string[]) => {
-  imageContainer.innerHTML = '';
+  image.innerHTML = '';
 
   urls.forEach((url) => {
-    imageContainer.append(img({ src: url, className: 'Avatar__layer' }));
+    image.append(img({ src: url, className: 'Avatar__layer' }));
   });
 };
 
@@ -63,7 +69,10 @@ const update = (name: string, property: 'url' | 'displayed', value: string | boo
   layers.update(name, property, value);
 };
 
-const toolPanel = new ToolPanel(document.body, elements, update);
+const toolPanelConatiner = div({ className: 'Avatar__toolPanelContainer' });
+avatarContainer.append(toolPanelConatiner);
+
+const toolPanel = new ToolPanel(toolPanelConatiner, elements, update);
 
 const saveButton = button({ onclick: () => svg.save(), className: 'button' }, ['Download']);
-document.body.append(saveButton);
+toolPanelConatiner.append(saveButton);
